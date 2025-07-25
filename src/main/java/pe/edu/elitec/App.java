cc755478ca13b7241aacb50f1167b6ad903bc421
Jhon Loyola <jhon.loyola@elitec.edu.pe>
2025-07-24 20:00:52 -0500

    Initial commit: Maven project setup

---
+++ src/main/java/pe/edu/elitec/App.java
@@ -0,0 +1,13 @@
+package pe.edu.elitec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
